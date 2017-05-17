0d9b103c39f5798f27f383546e1b154a345c7fcb
MechaKnightz <MechaKnightz@hotmail.com>
2017-05-17 21:04:15 +0200

    implemented autoscroll in the log that i found at http://stackoverflow.com/a/33045967

--- ServerGUI/MainWindow.xaml.cs
+++ ServerGUI/MainWindow.xaml.cs
@@ -105,5 +105,30 @@ namespace ServerGUI
                 BtnStop.IsEnabled = false;
             }
         }
+        //not mine
+        private void control_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            // If the entire contents fit on the screen, ignore this event
+            if (e.ExtentHeight < e.ViewportHeight)
+                return;
+
+            // If no items are available to display, ignore this event
+            if (ConsoleDataGrid.Items.Count <= 0)
+                return;
+
+            // If the ExtentHeight and ViewportHeight haven't changed, ignore this event
+            if (e.ExtentHeightChange == 0.0 && e.ViewportHeightChange == 0.0)
+                return;
+
+            // If we were close to the bottom when a new item appeared,
+            // scroll the new item into view.  We pick a threshold of 5
+            // items since issues were seen when resizing the window with
+            // smaller threshold values.
+            var oldExtentHeight = e.ExtentHeight - e.ExtentHeightChange;
+            var oldVerticalOffset = e.VerticalOffset - e.VerticalChange;
+            var oldViewportHeight = e.ViewportHeight - e.ViewportHeightChange;
+            if (oldVerticalOffset + oldViewportHeight + 2 >= oldExtentHeight)
+                ConsoleDataGrid.ScrollIntoView(ConsoleDataGrid.Items[ConsoleDataGrid.Items.Count - 1]);
+        }
     }
 }
